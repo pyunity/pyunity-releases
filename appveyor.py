3059f12c1fdfd7d0147b82f4a51ecbd36326aa3e
Ray Chen <tankimarshal2@gmail.com>
2021-04-22 16:57:38 +0100

    Releases for 0.3.0

--- appveyor.py
+++ appveyor.py
@@ -4,11 +4,11 @@ if os.path.exists("0.3.0"): shutil.rmtree("0.3.0")
 os.mkdir("0.3.0")
 
 for platform, plat_name in zip(
-        ["Visual%20Studio%202017", "Ubuntu", "macos"],
+        ["Visual%20Studio%202019", "Ubuntu", "macos"],
         ["win_amd64", "linux_x86_64", "macosx_10_15_x86_64"]):
     for version, name in zip(
-            ["3.6", "3.7", "3.8"],
-            ["cp36-cp36m-", "cp37-cp37m-", "cp38-cp38-"]):
+            ["3.6", "3.7", "3.8", "3.9"],
+            ["cp36-cp36m-", "cp37-cp37m-", "cp38-cp38-", "cp39-cp39-"]):
         venv = "../../venv" + version
         windows_location = "C:\\Python" + version[::2] + "-x64\\python"
         job_name = "Image:%20" + platform + ";%20Environment:%20version=" + version + ",%20venv=" + venv + ",%20windows_location=" + windows_location
