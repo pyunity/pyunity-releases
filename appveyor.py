cac733732b4cbccd74eeff4f0efefaa285015260
Ray Chen <tankimarshal2@gmail.com>
2021-06-17 17:44:28 +0100

    0.6.0 win32 releases

--- appveyor.py
+++ appveyor.py
@@ -1,19 +1,24 @@
+import urllib
 import wget, os, glob, shutil
 
 if os.path.exists("0.6.0"): shutil.rmtree("0.6.0")
 os.mkdir("0.6.0")
 
 for platform, plat_name in zip(
-        ["Visual%20Studio%202019", "Ubuntu", "macos"],
-        ["win_amd64", "linux_x86_64", "macosx_10_15_x86_64"]):
+        ["Visual%20Studio%202019", "Visual%20Studio%202017", "Ubuntu", "macos"],
+        ["win_amd64", "win32", "linux_x86_64", "macosx_10_15_x86_64"]):
     for version, name in zip(
             ["3.6", "3.7", "3.8", "3.9"],
             ["cp36-cp36m-", "cp37-cp37m-", "cp38-cp38-", "cp39-cp39-"]):
         venv = "../../venv" + version
         windows_location = "C:\\Python" + version[::2] + "-x64\\python"
-        job_name = "Image:%20" + platform + ";%20Environment:%20version=" + version + ",%20venv=" + venv + ",%20windows_location=" + windows_location
+        windows_location2 = "C:\\Python" + version[::2] + "\\python"
+        job_name = "Image:%20" + platform + ";%20Environment:%20version=" + version + ",%20venv=" + venv + ",%20windows_location=" + windows_location + ",%20windows_location2=" + windows_location2
         #print("https://ci.appveyor.com/api/projects/rayzchen/pyunity/artifacts/dist/pyunity-0.6.0-" + name + plat_name + ".whl?job=" + job_name)
-        wget.download("https://ci.appveyor.com/api/projects/rayzchen/pyunity/artifacts/dist/pyunity-0.6.0-" + name + plat_name + ".whl?job=" + job_name, "0.6.0/pyunity-0.6.0-" + name + plat_name + ".whl")
+        try:
+            wget.download("https://ci.appveyor.com/api/projects/rayzchen/pyunity/artifacts/dist/pyunity-0.6.0-" + name + plat_name + ".whl?job=" + job_name, "0.6.0/pyunity-0.6.0-" + name + plat_name + ".whl")
+        except urllib.error.HTTPError:
+            print("Couldnt download pyunity-0.6.0-" + name + plat_name + ".whl", end="")
         print()
 
 linux_builds = glob.glob("0.6.0/pyunity-0.6.0*linux*.whl")
