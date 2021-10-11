0c2ef230b8ce1357c75d25935724515847206192
Ray Chen <tankimarshal2@gmail.com>
2021-10-11 20:19:13 +0100

    Bump down to 0.8.1

--- appveyor.py
+++ appveyor.py
@@ -1,11 +1,11 @@
 import requests
 import wget, urllib, os, glob
 
-if os.path.exists("0.9.0"):
-    for file in glob.glob(os.path.join("0.9.0", "pyunity*.whl")):
+if os.path.exists("0.8.1"):
+    for file in glob.glob(os.path.join("0.8.1", "pyunity*.whl")):
         os.remove(file)
 else:
-    os.mkdir("0.9.0")
+    os.mkdir("0.8.1")
 
 apiUrl = "https://ci.appveyor.com/api"
 
@@ -18,10 +18,10 @@ for job in project.json()["build"]["jobs"]:
         file = artifact["fileName"]
         try:
             wget.download(f"{apiUrl}/buildjobs/{jobId}/artifacts/{file}",
-                "0.9.0/" + os.path.basename(file))
+                "0.8.1/" + os.path.basename(file))
         except urllib.error.HTTPError:
             print(f"Couldnt download {os.path.basename(file)}", end="")
         print()
 
-for file in glob.glob("0.9.0/*linux*"):
+for file in glob.glob("0.8.1/*linux*"):
     os.rename(file, file.replace("linux", "manylinux1"))
