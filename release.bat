c950349bfa6a26711053ee2773a98475d085abd7
Ray Chen <tankimarshal2@gmail.com>
2021-10-12 16:28:27 +0100

    0.8.2 bump

--- release.bat
+++ release.bat
@@ -2,5 +2,5 @@ python appveyor.py
 cd ../stubs
 py ../stubs/setup.py bdist_wheel -d ../dist/stubs sdist -d ../dist/stubs
 cd ../dist
-twine upload --repository testpypi 0.8.1/pyunity* stubs/pyunity_stubs-0.8.1*
-twine upload 0.8.1/pyunity* stubs/pyunity_stubs-0.8.1*
+twine upload --repository testpypi 0.8.2/pyunity* stubs/pyunity_stubs-0.8.2*
+twine upload 0.8.2/pyunity* stubs/pyunity_stubs-0.8.2*
