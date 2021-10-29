e8460b5deed2ce4d9e02f30be8c8925a36fb9729
Ray Chen <tankimarshal2@gmail.com>
2021-10-29 10:38:44 +0100

    Post2

--- release.bat
+++ release.bat
@@ -4,5 +4,5 @@ py setup.py sdist -d dist/0.8.3/
 cd stubs
 py setup.py bdist_wheel -d ../dist/stubs sdist -d ../dist/stubs
 cd ../dist
-twine upload --repository testpypi 0.8.3/pyunity* stubs/pyunity_stubs-0.8.3*
-twine upload 0.8.3/pyunity* stubs/pyunity_stubs-0.8.3*
+twine upload --repository testpypi 0.8.3/pyunity* stubs/pyunity?stubs*0.8.3*
+twine upload 0.8.3/pyunity* stubs/pyunity?stubs*0.8.3*
