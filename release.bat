a25ef0787348de2593573018f414e0340f02cead
Ray Chen <tankimarshal2@gmail.com>
2021-12-31 12:25:43 +0000

    0.8.4 sdist and stubs

--- release.bat
+++ release.bat
@@ -1,10 +1,12 @@
+@echo off
+
 python appveyor.py
 cd ..
-py setup.py sdist -d dist/post-0.4/0.8.4/
+py setup.py sdist -d dist\post-0.4\0.8.4\
 cd stubs
-py setup.py bdist_wheel -d ../dist/stubs sdist -d ../dist/stubs
-cd ../dist
-IF NOT [%1] == [] (
-twine upload --repository testpypi post-0.4/0.8.4/pyunity* stubs/pyunity?stubs*0.8.4*
-twine upload post-0.4/0.8.4/pyunity* stubs/pyunity?stubs*0.8.4*
+py setup.py bdist_wheel -d ..\dist\stubs sdist -d ..\dist\stubs
+cd ..\dist
+if not "%~1" == "" (
+twine upload --repository testpypi post-0.4\0.8.4\pyunity* stubs\pyunity?stubs*0.8.4*
+twine upload post-0.4\0.8.4\pyunity* stubs\pyunity?stubs*0.8.4*
 )
